f61762100ae1aefabe0e1abb4376b70df8f9d744
gabriel <gabosoft.ape@outlook.com>
2020-12-04 16:31:36 -0500

    udpate model

--- models/oportunities.py
+++ models/oportunities.py
@@ -6,18 +6,17 @@ from odoo import models, fields, api
 class motionOportunities(models.Model):
     _name = 'motion.crm_motion_oportunities'
     _description = 'Mensajes de contacto'
-    _rec_name = 'nombre'
+    _rec_name = 'empresa'
 
-    nombre = fields.Char('Nombre')
-    empresa = fields.Char('EMPRESA')
-    categoria = fields.Char('CATEGORIA')
+    empresa = fields.Char('Nombre de empresa', required=True)
+    categoria = fields.Many2one('motion.crm_motion_op_categ', string='Categoria', required=True)
     contacto = fields.Char('CONTACTO REF')
     cargo = fields.Char('CARGO REF')
     correo = fields.Char('CORREO')
     tel1 = fields.Char('TELEFONO 1')
     tel2 = fields.Char('TELEFONO 2')
     direccion = fields.Char('DIRECCIÓN')
-    comentarios = fields.Char('COMENTARIOS')
+    comentarios = fields.Text('COMENTARIOS')
     fecha = fields.Datetime('FECHA DE GESTIÓN INICIAL')
     state = fields.Selection(
         [('cancelado', 'Descartado'), ('nuevo', 'Prospecto'), ('sin concretar', 'Pendiente (sin concretar)'), ('concretado', 'Concretado')],
@@ -27,3 +26,12 @@ class motionOportunities(models.Model):
     def create(self, vals):
         result = super(motionOportunities, self).create(vals)
         return result
+
+
+class motionOportunities(models.Model):
+    _name = 'motion.crm_motion_op_categ'
+    _description = 'Categoria Oportunidad'
+    _rec_name = 'empresa'
+
+    categoria = fields.Char('Nombre de empresa', required=True)
+    descripcion = fields.Text('Descripcion')
