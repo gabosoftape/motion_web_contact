34401d84efd157b37033cc0827b424fd27776046
gabriel <gabosoft.ape@outlook.com>
2020-12-04 15:59:07 -0500

    test new class for testing

---
+++ models/oportunities.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*- #
+import time
+from odoo import models, fields, api
+
+
+class motionContact(models.Model):
+    _name = 'motion.crm_motion_oportunities'
+    _description = 'Mensajes de contacto'
+    _rec_name = 'nombre'
+
+    nombre = fields.Char('Nombre')
+    empresa = fields.Char('EMPRESA')
+    categoria = fields.Char('CATEGORIA')
+    contacto = fields.Char('CONTACTO REF')
+    cargo = fields.Char('CARGO REF')
+    correo = fields.Char('CORREO')
+    tel1 = fields.Char('TELEFONO 1')
+    tel2 = fields.Char('TELEFONO 2')
+    direccion = fields.Char('DIRECCIÓN')
+    comentarios = fields.Char('COMENTARIOS')
+    fecha = fields.Char('FECHA DE GESTIÓN INICIAL')
+    state = fields.Selection(
+        [('cancelado', 'Descartado'), ('nuevo', 'Prospecto'), ('sin concretar', 'Pendiente (sin concretar)'), ('concretado', 'Concretado')],
+        'Estado', default='nuevo')
+
+    @api.model
+    def create(self, vals):
+        result = super(motionContact, self).create(vals)
+        return result
